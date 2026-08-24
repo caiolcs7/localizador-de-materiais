8527030e5ba33a0c7447ca242730ac61a5daae5b
caiolcs7 <caio.lucas100@gmail.com>
2026-08-24 15:08:40 -0300

    Mark successful app boot

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
 import App from './App'
+
+declare global { interface Window { __LM_BOOT_OK__?: boolean } }
+window.__LM_BOOT_OK__ = true
 registerSW({ immediate: true })
 createRoot(document.getElementById('root')!).render(<StrictMode><App/></StrictMode>)
