6a0421f3d795e3b43ab0e18afd16ed9cbfeac1ee
caiolcs7 <caio.lucas100@gmail.com>
2026-09-02 11:41:49 -0300

    style: load status enhancement styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
 import App from './App'
+import './status-enhancements.css'
 
 declare global { interface Window { __LM_BOOT_OK__?: boolean } }
 window.__LM_BOOT_OK__ = true
